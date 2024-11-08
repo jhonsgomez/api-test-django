c27ea401a10ae0d172486e5ff6b71e7bb4be861d
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2024-11-08 15:23:56 -0500

    setup database

--- project/settings.py
+++ project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = "project.wsgi.app"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "aWWLTquZNckDiTmffxDfCtzjSRXKwbBt",
+        "HOST": "autorack.proxy.rlwy.net",
+        "PORT": "44855",
     }
 }
 
