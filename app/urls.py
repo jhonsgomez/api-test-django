072d754992a56ba9893d3afd5e15fd3bb1c91d53
jhonsgomez <jhonsebastiangomezsierra@gmail.com>
2024-11-08 14:43:10 -0500

    first commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path("messages/", view=views.get_messages, name="get-messages"),
+    path("messages/create/", view=views.create_message, name="create-message"),
+]
